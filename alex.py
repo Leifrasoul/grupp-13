51e104fe549b41546099760b5455e9d1ce99d855
AlexRadic <alexradic242@gmail.com>
2025-03-27 10:30:45 +0100

    Update alex.py

--- alex.py
+++ alex.py
@@ -1,5 +1,12 @@
 def hello():
     print("Hej mitt namn är Alex")
+<<<<<<< Updated upstream
     
 def drink():
     print("Mn favorit dricka är juice")
+=======
+
+def food():
+    print("Min favorit mat är tacos")
+
+>>>>>>> Stashed changes
