ac1ec8ef00c21de8e3f34942c594c81ebd1f60ff
Leifrasoul <leifrasoul@gmail.com>
2025-03-27 09:50:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import leif
-print("Mata in ett tal")
+import leif 
+import alex
+def main():
+    namn = input("Vem vill du säga hej till? ").lower()
 
-input("")
+    if namn == "leif":
+        leif.hello()
+    elif namn == "alex":
+        alex.hello()
+    else:
+        print("Ingen sådan person ")
 
-Hej = input
-
+if __name__ == "__main__":
+    main()
