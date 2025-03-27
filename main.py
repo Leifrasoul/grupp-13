5dd4767e71e8f4e65bb6454d223513ce87c284ac
Leifrasoul <leifrasoul@gmail.com>
2025-03-27 10:16:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import leif 
 import alex
+import sebbe
+import alex
+
 def main():
-    user_input = input("Vem vill du prata med").lower().split()
+    user_input = input("Skriv t.ex.  'leif food', 'alex drink' eller bara 'leif': ").lower().split()
 
     if len(user_input) == 1:
         name = user_input[0]
@@ -17,13 +20,21 @@ def main():
             leif.hello()
         elif action == "food":
             leif.food()
+        elif action == "drink":
+            leif.drink()
+        else:
+            print("Okänd funktion för Sebbe")
     elif name == "alex":
         if action == "hello":
             alex.hello()
         elif action == "food":
             alex.food()
+        elif action == "drink":
+            alex.drink()
+        else:
+            print("Okänd funktion för Alex")
     else:
-        print("Ingen sådan person ")
+        print("Ingen sådan person")
 
 if __name__ == "__main__":
     main()
