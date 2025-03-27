2a1108ef5513da30201372febf79d1746f6ac396
Iammar2005 <ammar.ime.05@gmail.com>
2025-03-27 10:23:57 +0100

    Update ammar.py

--- ammar.py
+++ ammar.py
@@ -1,2 +1,13 @@
+<<<<<<< Updated upstream
 print ("Hi, I'm Ammar")
 print ("Hej")
+=======
+def hello():
+    print ("Hi, I'm Ammar")
+
+def food():
+    print("Min favorit mat är burek")
+
+def drink():
+    print("Min favorit dricka är Cola")
+>>>>>>> Stashed changes
